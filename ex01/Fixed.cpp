fb1496fb082a1a19a9a3c16a48fa7117810509dc
Yeeun Ju <yeju@c4r2s8.42seoul.kr>
2022-03-31 18:40:13 +0900

    cpp02

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -3,63 +3,52 @@
 const int	Fixed::_bits = 8;
 
 Fixed::Fixed(void) {
-	_value = 0;
 	std::cout << "Default constructor called" << std::endl;
+	_value = 0;
 }
 
-Fixed::Fixed(const int value)
-{
-	_value = value << _bits;
+Fixed::Fixed(const int value) {
 	std::cout << "Int constructor called" << std::endl;
+	_value = value << _bits;
 }
 
-Fixed::Fixed(const float value)
-{
-	_value = roundf(value * (1 << _bits));
+Fixed::Fixed(const float value) {
 	std::cout << "Float constructor called" << std::endl;
+	_value = roundf(value * (1 << _bits));
 }
 
-Fixed::Fixed(const Fixed &rhs)
-{
-	std::cout << "Copy constructor called" << std::endl;
+Fixed::Fixed(const Fixed &rhs) {
 	*this = rhs;
+	std::cout << "Copy constructor called" << std::endl;
 }
 
-Fixed::~Fixed(void)
-{
+Fixed::~Fixed(void) {
 	std::cout << "Destrutor called" << std::endl;
 }
 
-Fixed&	Fixed::operator=(const Fixed &rhs)
-{
+Fixed&	Fixed::operator=(const Fixed &rhs) {
 	std::cout << "Assignation operation called" << std::endl;
-
 	_value = rhs._value;
 	return (*this);
 }
 
-int	Fixed::getRawBits(void) const
-{
+int	Fixed::getRawBits(void) const {
 	return (_value);
 }
 
-void	Fixed::setRawBits(int const raw)
-{
+void	Fixed::setRawBits(int const raw) {
 	_value = raw;
 }
 
-int	Fixed::toInt(void) const
-{
+int	Fixed::toInt(void) const {
 	return (_value >> _bits);
 }
 
-float	Fixed::toFloat(void) const
-{
+float	Fixed::toFloat(void) const {
 	return (float(_value) / (1 << _bits));
 }
 
-std::ostream&	operator<<(std::ostream &out, const Fixed &fixed)
-{
+std::ostream&	operator<<(std::ostream &out, const Fixed &fixed) {
 	out << fixed.toFloat(); //고정 소수점 값을 toFloat 함수를 통해 부동 소수점으로 출력
 	return out;
 }
--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -5,25 +5,24 @@
 #include <cmath>
 
 class Fixed {
+private:
+	int					_value;
+	const static int	_bits;
 public:
 	Fixed(void);
-	Fixed(const int fixed); //상수 정수를 매개변수로 사용하고 이를 해당 fixed(8) 포인트 값으로 변환하는 생성자. 값은 0으로 초기화
-	Fixed(const float fixed); //상수 부동 소수점을 매개변수로 사용하고 해당 고정(8) 소수점 값으로 변환하는 생성자. 값은 0으로 초기화
+	Fixed(const int fixed);
+	Fixed(const float fixed);
 	Fixed(const Fixed &rhs);
-	~Fixed(void);
-
-	Fixed &	operator=(Fixed const &rhs);
+	Fixed &	Fixed::operator=(Fixed const &rhs);
+	~Fixed(void)
 
 	int		getRawBits(void) const;
 	void	setRawBits(int const raw);
 	
-	float	toFloat(void) const; //고정 소수점 값을 부동 소수점 값으로 변환합니다.
-	int		toInt(void) const; //고정 소수점 값을 정수 값으로 변환합니다.
-private:
-	int					_value;
-	const static int	_bits;
+	float	toFloat( void ) const;
+	int		toInt( void ) const;
 };
 
-std::ostream &	operator<<(std::ostream & s, const Fixed & fixed); //고정 소수점 값의 부동 소수점 표현을 매개변수 출력 스트림에 삽입하는 « 연산자에 대한 오버로드.
+std::ostream &	operator<<(std::ostream & s, const Fixed & fixed);
 
 #endif
